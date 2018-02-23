a68b99b6b95582cf3bd9f44510f46cc8ef3501d6
Andy Hunt <andy.l.hunt85@gmail.com>
2018-02-23 16:03:57 -0500

    working undo buttons

--- script.js
+++ script.js
@@ -1,26 +1,80 @@
 $(document).ready(function() {
 
 /* Global Variables */
+  let screen = document.getElementById('screen')
 
-  let num = 0;
+  var total = 0;
+
+
+  // Basic Operand Functions
 
   function add(num) {
-    num += 5;
-    console.log(num)
+    total += num;
+    console.log(total);
+  }
+
+  function subtract(num) {
+    total -= num;
+    console.log(total);
+  }
+
+  function multiply(num) {
+    total *= num;
+    console.log(total);
+  }
+
+  function divide(num) {
+    total /= num;
+    console.log(total);
+  }
+
+  // Displays On Screen
+
+  function display() {
+    $('.number-button').click(function() {
+      if (total === 0) {
+        total = $(this).val();
+      } else {
+        total += $(this).val();
+      }
+      screen.textContent = total;
+    })
+  }
+
+  function operate(total, current, operand) {
+
   }
 
 
+  //Clears everything
+  function clearAll() {
+    $('#clear-all').click(function() {
+      total = 0;
+      screen.textContent = total;
+      return total;
+    });
+  };
+
+  //Undoes The Last Number
+  function undo() {
+    $('#undo').click(function() {
+      if (total == 0) {
+        return 0;
+      } else {
+        total = total.split('');
+        total.pop()
+        total = total.join('');
+        if (total.length === 0) {
+          total = 0;
+        }
+        screen.textContent = total;
+      }
+    })
+  }
 
 
-
-
-
-
-
-
-
-
-
-
+display();
+clearAll();
+undo();
 
 })
